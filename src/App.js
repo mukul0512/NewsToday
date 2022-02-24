0477ca68713d29ecbb69264bbede4309937c9be5
mukul0512 <karnwal.mukul@gmail.com>
2022-02-24 15:23:42 +0530

    Changes to be staged router reimported

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 
 const App = () => {
@@ -18,16 +18,16 @@ const App = () => {
         progress={progress}
       />
       <Navbar />
-      <News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />
+      {/* <News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />
       <News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />
       <News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />
       <News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />
       <News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />
       <News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />
       <News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />
-      <News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />
-      
-      {/* <Routes>
+      <News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" /> */}
+
+      <Routes>
         <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
         <Route path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
         <Route path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
@@ -36,7 +36,7 @@ const App = () => {
         <Route path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
         <Route path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
         <Route path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
-      </Routes> */}
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -7,9 +7,8 @@ const Navbar = () => {
         <div>
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    {/* <Link className="navbar-brand" to="/">NewsToday</Link> */}
-                    <a className="navbar-brand" href="#">NewsToday</a>
-
+                    <Link className="navbar-brand" to="/">NewsToday</Link>
+                    {/* <a className="navbar-brand" href="#">NewsToday</a> */}
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -18,46 +17,46 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                {/* <Link className="nav-link" aria-current="page" to="/">Home</Link> */}
-                                <a className="nav-link" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                {/* <a className="nav-link" aria-current="page" href="#">Home</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link business className="nav-link" href="/business">Business</Link> */}
-                                <a business className="nav-link" href="#business">Business</a>
+                                <Link business className="nav-link" href="/business">Business</Link>
+                                {/* <a business className="nav-link" href="/business">Business</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link entertainment className="nav-link" href="/entertainment">Entertainment</Link> */}
-                                <a entertainment className="nav-link" href="#entertainment">Entertainment</a>
+                                <Link entertainment className="nav-link" href="/entertainment">Entertainment</Link>
+                                {/* <a entertainment className="nav-link" href="/entertainment">Entertainment</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link general className="nav-link" href="/general">General</Link>
-                                 */}
-                                <a general className="nav-link" href="#general">General</a>
+                                <Link general className="nav-link" href="/general">General</Link>
+                                
+                                {/* <a general className="nav-link" href="/general">General</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link health className="nav-link" href="/health">Health</Link>
-                                 */}
-                                <a health className="nav-link" href="#health">Health</a>
+                                <Link health className="nav-link" href="/health">Health</Link>
+                                
+                                {/* <a health className="nav-link" href="/health">Health</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link science className="nav-link" href="/science">Science</Link> */}
-                                <a science className="nav-link" href="#science">Science</a>
+                                <Link science className="nav-link" href="/science">Science</Link>
+                                {/* <a science className="nav-link" href="/science">Science</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link sports className="nav-link" href="/sports">Sports</Link>
-                                 */}
-                                <a sports className="nav-link" href="#sports">Sports</a>
+                                <Link sports className="nav-link" href="/sports">Sports</Link>
+                                
+                                {/* <a sports className="nav-link" href="/sports">Sports</a> */}
                             </li>
 
                             <li className="nav-item">
-                                {/* <Link technology className="nav-link" href="/technology">Technology</Link> */}
-                                <a technology className="nav-link" href="#technology">Technology</a>
+                                <Link technology className="nav-link" href="/technology">Technology</Link>
+                                {/* <a technology className="nav-link" href="/technology">Technology</a> */}
                             </li>
 
                         </ul>
